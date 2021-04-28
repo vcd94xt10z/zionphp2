c6db376d1b144f2391977e67f98b1306aa7855c7
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2021-04-28 09:08:50 -0300

    .

--- app-kit/index.php
+++ app-kit/index.php
@@ -1,3 +1,8 @@
 <?php
+$zionphp2 = [
+    "configFile" => "c:\webserver\sites\localhost\config.json"
+];
 require(dirname(dirname(dirname(__FILE__)))."/zionphp2/autoload.php");
+
+echo "Rotas";
 ?>
--- autoload.php
+++ autoload.php
@@ -1,4 +1,12 @@
 <?php
-echo "zionphp2";
-exit();
+// A configuração não é obrigatória, porém, se não for feita, não poderá ser utilizado todos os recursos
+// como persistência por exemplo.
+if(is_array($zionphp2)){
+    $file = $zionphp2["configFile"];
+    if(!file_exists($file)){
+        http_response_code(500);
+        echo "O arquivo de configuração ".$file." não existe";
+        exit();
+    }
+}
 ?>
