2cb93616e6d8cf5ede2210c83eb1556431c8e215
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2021-04-29 09:03:52 -0300

    .

---
+++ functions.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Este arquivo deve conter funções que estaram disponível no escopo global
+ * Coloque aqui as funções para ajustar algum comportamento incorreto
+ */
+
+/**
+ * Lê a configuração dos arquivos
+ * @return []
+ */
+function zion_get_config_all(){
+    $all = [];
+    
+    // configuração básica
+    $json = zion_get_config("config.json",true);
+    $json = is_array($json)?$json:array();
+    foreach($json AS $key => $value){
+        $all[$key] = $value;
+    }
+    
+    // configuração do ambiente
+    $json = zion_get_config(\zion\ENV.".json",false);
+    $json = is_array($json)?$json:array();
+    foreach($json AS $key => $value){
+        $all[$key] = $value;
+    }
+    
+    return $all;
+}
+
+/**
+ * Lê a configuração de um arquivo
+ * @param string $filename
+ * @param boolean $stopOnError
+ * @return []
+ */
+function zion_get_config($filename,$stopOnError=true){
+    $file = dirname($_SERVER["DOCUMENT_ROOT"])."/config/".$filename;
+    if(!file_exists($file)){
+        if(!$stopOnError){
+            return;
+        }
+        
+        http_response_code(500);
+        echo "Arquivo de configuração {$filename} não encontrado";
+        exit();
+    }
+    
+    $json = json_decode(file_get_contents($file),true);
+    if(!is_array($json)){
+        return;
+    }
+    
+    return $json;
+}
+
+/**
+ * Autoload do sistema
+ * @param string $className
+ * @return boolean
+ */
+function zionphp_autoload($className) {
+    // zion: módulos
+    if(strpos($className, "zion\\mod\\") === 0) {
+        $className = str_replace("\\","/",$className);
+        $className = str_replace("zion/mod/","modules/",$className);
+        $file       = \zion\ROOT.$className.".class.php";
+        
+        if(file_exists($file)) {
+            require_once($file);
+            return true;
+        }
+        return false;
+    }
+    
+    // zion: framework / biblioteca
+    $className2 = str_replace("zion\\","src\\zion\\",$className);
+    $file = \zion\ROOT.str_replace("\\","/",$className2).".class.php";
+    if(file_exists($file)) {
+        require_once($file);
+        return true;
+    }
+    
+    // app: biblioteca
+    if(strpos($className, "app\\") === 0){
+        $folder = rtrim(dirname($_SERVER["DOCUMENT_ROOT"]))."/app/";
+        $file = str_replace("app\\",$folder,$className).".class.php";
+        $file = str_replace("\\","/",$file);
+        if(file_exists($file)){
+            require_once($file);
+            return true;
+        }
+        return false;
+    }
+    
+    // app: biblioteca
+    if(strpos($className, "lib\\") === 0){
+        $folder = rtrim(dirname($_SERVER["DOCUMENT_ROOT"]))."/src/";
+        $file = str_replace("src\\",$folder,$className).".class.php";
+        $file = str_replace("\\","/",$file);
+        if(file_exists($file)){
+            require_once($file);
+            return true;
+        }
+        return false;
+    }
+    
+    // app: módulos
+    if(strpos($className, "mod\\") === 0){
+        $parts = explode("\\", $className);
+        $parts[0] = "modules";
+        
+        $file = rtrim($_SERVER["DOCUMENT_ROOT"])."/".implode("/", $parts).".class.php";
+        if(file_exists($file)){
+            require_once($file);
+            return true;
+        }
+        return false;
+    }
+    
+    return false;
+}
+
+/**
+ * Função de callback para desserialização
+ * @param string $className
+ */
+function zion_unserialize_callback_func($className){
+    foreach(spl_autoload_functions() AS $function){
+        $result = $function($className);
+        if($result){
+            return;
+        }
+    }
+}
+
+function old_count($arg){
+    if(is_array($arg)){
+        return count($arg);
+    }else{
+        return 0;
+    }
+}
+?>
---
+++ php/core.php
@@ -0,0 +1,35 @@
+<?php
+namespace {
+    class OAuth {}
+    class ntlm_sasl_client_class {}
+    class mime_parser_class {}
+}
+
+namespace League\OAuth2\Client\Grant {
+    class RefreshToken {}
+}
+
+namespace League\OAuth2\Client\Provider {
+    class AbstractProvider {}
+}
+
+namespace  League\OAuth2\Client\Token {
+    class AccessToken {}
+}
+
+namespace Psr\Log {
+    class LoggerInterface {}
+}
+
+namespace League\OAuth2\Client\Provider {
+    class Google {}    
+}
+
+namespace Hayageek\OAuth2\Client\Provider {
+    class Yahoo {}
+}
+
+namespace Stevenmaguire\OAuth2\Client\Provider {
+    class Microsoft {}
+}
+?>
