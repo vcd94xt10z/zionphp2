8f6397a61626c55f5c747c31706cc20bc78a586f
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2021-04-29 09:13:08 -0300

    .

--- app-kit/webserver/sites/localhost/public/index.php
+++ app-kit/webserver/sites/localhost/public/index.php
@@ -1,6 +1,6 @@
 <?php
 $zionphp2 = [
-    "configFile" => dirname($_SERVER["DOCUMENT_ROOT"])."/config.json"
+    "configFile" => dirname($_SERVER["DOCUMENT_ROOT"])."/config/config.json"
 ];
 require(dirname(dirname(dirname(__FILE__)))."/zionphp2/autoload.php");
 
